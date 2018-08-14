146eef4c8b7b46798c1cc695089259cdc1eecda5
baobaojianqiang <16230091@qq.com>
2018-08-14 09:49:09 +0800

    fix bug

--- mypluginlibrary/src/main/java/com/example/jianqiang/mypluginlibrary/Itest.java
+++
@@ -1,8 +0,0 @@
-package com.example.jianqiang.mypluginlibrary;
-
-/**
- * Created by baojianqiang on 16/2/23.
- */
-public abstract interface Itest{
-    public abstract void test();
-}
--- mypluginlibrary/src/main/java/com/example/jianqiang/mypluginlibrary/YKCallBack.java
+++
@@ -1,8 +0,0 @@
-package com.example.jianqiang.mypluginlibrary;
-
-/**
- * Created by baojianqiang on 16/2/23.
- */
-public abstract interface YKCallBack{
-    public abstract void callback(IBean paramIBean);
-}
